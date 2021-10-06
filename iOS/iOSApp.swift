79e4704c3c154dfcc19b025d6c29c63551276d51
Steven Zhang <YZhang@lcs.on.ca>
2021-10-06 09:43:52 -0400

    Initial Commit

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,43 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Russell Gordon on 2021-10-02.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    
+    @StateObject private var viewModel = WeatherViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            
+            TabView {
+                //Call site (Where you create an instance of someting if youare calling the initializer)
+                NavigationView {
+                    ContentView(viewModel: viewModel,
+                                temperature: viewModel.history.last!.temperature,
+                                feel: viewModel.history.last!.feel,
+                                conditions: viewModel.history.last!.condition.description)
+                }
+                .tabItem {
+                    Image(systemName: "sun.haze.fill")
+                    Text("Current")
+                }
+
+                NavigationView {
+                    HistoryView(viewModel: viewModel)
+                }
+                .tabItem {
+                    Image(systemName: "clock.fill")
+                    Text("History")
+                }
+
+            }
+            
+        }
+    }
+}
